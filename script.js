e76de460c48ee336adc94cab60db2279ecae8fab
ranita <ranitalayek99@gmail.com>
2022-02-08 22:11:26 +0530

    index

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let js ='amazing';
         //if(js==='amazing')
-           alert('Its FUN time');
+           //alert('Its FUN time');
 
            console.log(40+10);
            /*
@@ -13,8 +13,38 @@ let js ='amazing';
            let firstName="ram";
            console.log(firstName);
 
+           let javascriptIsFun=true;
+           console.log(javascriptIsFun);
+
            let myFirstJob ='Assistant';
            let mySecondJob="Havard";
 
            console.log(myFirstJob);
            console.log(mySecondJob);
+
+           console.log(typeof true);
+           console.log(typeof javascriptIsFun);
+           console.log(typeof 23);
+           console.log(typeof "Havard");
+
+           javascriptIsFun='RAM';
+           console.log(typeof javascriptIsFun);
+
+           //undefined means empty value
+
+           let year;
+           console.log(year);
+           console.log(typeof year);
+
+           year=1992;
+           console.log(year);
+           console.log(typeof year);
+
+           console.log(typeof null);
+
+           /*const a=10;
+           a=30;  not possible due to const variable
+           
+           var a=10;
+           a=40;*/
+
