2cb70c30c007eb157cb04b0d0eedd206998ecda7
ranita <ranitalayek99@gmail.com>
2022-02-08 20:28:40 +0530

    indjs

---
+++ script.js
@@ -0,0 +1,20 @@
+let js ='amazing';
+        //if(js==='amazing')
+           alert('Its FUN time');
+
+           console.log(40+10);
+           /*
+         Multiline commnet
+
+           */
+
+           console.log('Jonas');
+
+           let firstName="ram";
+           console.log(firstName);
+
+           let myFirstJob ='Assistant';
+           let mySecondJob="Havard";
+
+           console.log(myFirstJob);
+           console.log(mySecondJob);
