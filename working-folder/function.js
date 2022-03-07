1e682825a3cc79462e58cf021423afd54996b4c9
ranita <ranitalayek99@gmail.com>
2022-03-07 12:23:58 +0530

    add

--- working-folder/function.js
+++ working-folder/function.js
@@ -18,3 +18,40 @@ console.log(juiceMug);
 
 const appleOrangeJuice=foodProcessor(4,10);
 console.log(appleOrangeJuice);
+
+//Regular Functions & Arrow Function
+
+const ray={
+    year:1990,
+    firstName:'suraj',
+
+    calcAge:function(){
+       // console.log(this);
+        console.log(2040-this.year);
+    const isMillian=function(){
+        console.log(this);
+        console.log(this.year>=1980 && this.year<=1995);
+    }
+    isMillian();
+    },
+
+greet:()=>{
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+}
+}
+ray.greet();
+ray.calcAge();
+
+//An arrow function inherit functions from parent scope
+
+//Argument keywords
+
+const exPrt=function(a,b){
+    console.log(arguments);
+    return a+b;
+}
+
+exPrt(2,5);
+//o/p will be arrow
+
