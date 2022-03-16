036fc5d312e731bbc24e34b914c57501dd74dac4
ranita <ranitalayek99@gmail.com>
2022-03-16 18:45:49 +0530

    add

--- working-folder/datastructure/sets.js
+++ working-folder/datastructure/sets.js
@@ -10,7 +10,33 @@ const orderSet=new Set([
 
 console.log(orderSet);
 
-//strings are also iterable
+//strings,sets are also iterable
 
 console.log(new Set('jonas'));
-console.log(orderSet.size);
+console.log(orderSet.has('pizza'));
+//has is a method inbuilt its similar to includes method
+console.log(orderSet.has('pasta'));
+orderSet.add('Garlic bread');
+orderSet.add('Garlic bread');
+orderSet.delete('picaso');
+console.log(orderSet);
+
+for(const order of orderSet){
+    console.log(order);
+}
+
+//sets use for remove duplicate values
+
+const staff=['A','B','C','A','C'];
+//set to Array convert
+const staffSet=[...new Set(staff)];
+    console.log(staffSet);
+//console.log(new Set['A','B','C','A','C'].size
+
+
+
+
+
+
+
+
