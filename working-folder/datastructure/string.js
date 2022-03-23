3c3d24241a39814f63d60fe5d0e8633bcdece7a4
ranita <ranitalayek99@gmail.com>
2022-03-23 10:21:40 +0530

    add

---
+++ working-folder/datastructure/string.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const airline='Air Asia';
+const airplane='AAE01';
+
+console.log(airplane[0]);
+console.log(airplane[1]);
+console.log(airplane[2]);
+
+//string direct o/p
+console.log('BB293'[0]);
+
+//length property from a string
+
+console.log(airplane.length);
+
+//comparing string to Array
+console.log(airline.indexOf('s'));
+//console.log(airline.lastIndexOf('s'));
+console.log(airline.indexOf('Asia') );
