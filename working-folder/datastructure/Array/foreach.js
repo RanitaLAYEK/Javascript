3cf2659ea4fac61caebe7d17b04eeb4ac3c8e2e7
ranita <ranitalayek99@gmail.com>
2022-03-30 16:13:22 +0530

    add

--- working-folder/datastructure/Array/foreach.js
+++ working-folder/datastructure/Array/foreach.js
@@ -1,3 +1,4 @@
+'use strict'
 const movements=[200, 450, -400, 3000, -650, -130, 70, 1300];
 
 //for (const movement of movements){
@@ -6,29 +7,30 @@ const movements=[200, 450, -400, 3000, -650, -130, 70, 1300];
     if(movements>0){
         console.log(`Movement ${i+1}:You deposited ${movement}`);
     }else{
-        console.log(`Movement ${i+1}:You withdrew ${Math.abs(movement)}`);//abs-absolute value
+        console.log(`Movement ${i+1}:You withdrew ${Math.abs(movement)}`);
+        
     }
 }
 
-console.log("================================================");
-movements.forEach(function(movement,index,array){ //foreach --needs a callback function,passes the current element of array 
-                        //current element,currrent index,current array
+console.log('================================================');
+
+movements.forEach(function(movement,index,array){ 
         if(movements>0){
-            console.log(`Movement ${index+1}:You deposited ${movement}`);
+            console.log(`Movement ${index+1}:You deposited ${movement}`); 
+                        //current element,currrent index,current array
         }else{
-            console.log(`Movement ${index+1}: You withdrew ${Math.abs(movement)}`);//abs-absolute value
+            console.log(`Movement ${index+1}: You withdrew ${Math.abs(movement)}`);
         }
-    })
+    });
 
-// 0: function(200)
-// 1: function(450)
-// 2: function(400)
-// ...
-
-//foreach loop break statement dont work
-
-// forEach With Maps and Sets
-// Map
+/* 0: function(200)
+ 1: function(450)
+ 2: function(400)
+ ...
+abs-absolute value
+foreach loop break statement dont work 
+foreach --needs a callback function,passes the current element of array
+forEach With Maps and Sets */
 
 const currencies=new Map([
     ['USD','United States dollar'],
@@ -40,5 +42,13 @@ currencies.forEach(function(value,key,map){
     console.log(`${key}: ${value}`);
 })
 
+//SET
 
+// Set
+const currenciesUnique1 = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique1);
+currenciesUnique1.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
 
+//passing iterables like []
