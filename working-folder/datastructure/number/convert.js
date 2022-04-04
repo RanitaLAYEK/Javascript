6faabf7cb2d637850bcdc0469b36fd9bc0c26c1c
ranita <ranitalayek99@gmail.com>
2022-04-04 11:52:12 +0530

    add

--- working-folder/datastructure/number/convert.js
+++ working-folder/datastructure/number/convert.js
@@ -1,2 +1,31 @@
 'use strict'
 
+console.log(23===23.0);
+
+//binary base -2 (0,1)
+//base 10-0 to 9 
+
+console.log(0.1+0.2);
+console.log(0.1+0.2===0.3);//false
+
+console.log(Number('23'));//23 
+console.log(+'23');//23
+
+//parsing--parse a number to string
+console.log(Number.parseInt('30px'));//30
+console.log(Number.parseInt('e30'));//Nan
+console.log(Number.parseFloat('3.8rem'));//3.8
+console.log(Number.parseInt('3.8rem'));//3
+
+//NaN==not a number==to check any value is a number(like isNaN);
+
+console.log(Number.isNaN(23));//false
+console.log(Number.isNaN('23'));//false
+console.log(Number.isNaN(+'20x'));//true
+
+console.log(Number.isFinite(20));
+console.log(Number.isInteger(23));
+
+
+
+
---
+++ working-folder/datastructure/number/math-rounding.js
@@ -0,0 +1,7 @@
+'use strict'
+
+console.log(Math.sqrt(4));//squareroot
+console.log(2**(1/2));
+console.log(8**(1/3));
+
+console.log(Math.max(11,29,34,56,1));
