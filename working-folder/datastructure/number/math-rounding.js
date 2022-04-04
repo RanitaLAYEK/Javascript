c5a2c302cbdfcf0b7c2362255c888e2c7fd7d2f5
ranita <ranitalayek99@gmail.com>
2022-04-04 13:14:21 +0530

    add

--- working-folder/datastructure/number/math-rounding.js
+++ working-folder/datastructure/number/math-rounding.js
@@ -5,3 +5,36 @@ console.log(2**(1/2));
 console.log(8**(1/3));
 
 console.log(Math.max(11,29,34,56,1));
+console.log(Math.max(24,27,29,'40',30));
+console.log(Math.min(11,29,34,56,1));
+
+//Area of a circle
+console.log(Math.PI*Number.parseFloat('10px')**2);
+console.log(Math.trunc(Math.random*6)+1);//till 4 random number will generate
+
+//Generate random number
+const randomList=(max,min)=>(Math.random()*(max-min)+1);
+console.log(randomList(10,20));
+
+//Rounding integer
+console.log(Math.trunc(23.3));
+console.log(Math.round(23.8));
+
+console.log(Math.ceil(23.8));
+console.log(Math.floor(23.8));
+console.log(Math.round(23.8));
+
+//Rounding Decimal
+//console.log((2,7).tofix(0));//it return a string
+
+//Boxing in javascript
+
+//Reminder operator==return reminder of a division
+console.log(3%2);
+
+//EVEN OR ODD Number
+
+const isEven=n=>(n%2===0);
+console.log(isEven(10));
+console.log(isEven(21));
+console.log(isEven(8));
